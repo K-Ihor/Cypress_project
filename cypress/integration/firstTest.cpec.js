d41a4fd479f0d5486ddde3ef51feea296b1e5c7a
Igor Kucherov <ikucherov@atncorp.com>
2023-09-18 18:11:17 +0300

    check contains

--- cypress/integration/firstTest.cpec.js
+++ cypress/integration/firstTest.cpec.js
@@ -1,1 +1,23 @@
 /// <reference types="Cypress" />
+
+it('name_test', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in')
+})
+
+it('', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div', 'Sign in')
+})
+
+it('', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN IN', {matchCase: false})  // без учета регистра
+})
+
+
+it.only('', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('footer').contains('for incomming calls from abroad')
+})
+
